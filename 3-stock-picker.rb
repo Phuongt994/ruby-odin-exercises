1f063bb06328b215a5859c593c55df2675f2d66b
Phuong A T <phuongt994@gmail.com>
2020-11-26 15:49:17 +0700

    add checker when stock price at last index

--- 3-stock-picker.rb
+++ 3-stock-picker.rb
@@ -4,7 +4,7 @@
 def stock_picker(prices)
     max_profit, profit, sell_price, buy_price = 0
     prices.each_with_index do |price, index|
-        if prices[index] < prices.length - 1 && price < prices[index+1] 
+        if index < prices.length - 1 && price < prices[index+1] 
             focus_prices = prices[index+1..prices.length]
             focus_prices.each do |potential_sell_price|
                 profit = potential_sell_price - price
@@ -14,10 +14,12 @@ def stock_picker(prices)
                     buy_price = price
                 end
             end
+        else  
+            return "best buy price is at last index!"
         end
     end
     return max_profit, sell_price, buy_price
 end
 
-p stock_picker([17,10,6,9,15,8,6,1,3])
+p stock_picker([17,15,14,13,12,11,10,9,1])
 # => [1,4]  # for a profit of $15 - $3 == $12
